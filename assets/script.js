8d3f61e64d696f1891ea1e0c04e3bd6f1d740421
Combatxd <logan.schoerner@gmail.com>
2022-07-24 16:59:37 -0600

    workingConsoleCalculationForConvert

--- assets/script.js
+++ assets/script.js
@@ -11,12 +11,13 @@ var requestOptions = {
   headers: myHeaders
 };
 
+// Handles all current rate checks
 
 function searchApi(currBase, currSymb) {
 
   fetch("https://api.apilayer.com/exchangerates_data/latest?symbols="+ currSymb +"&base=" + currBase, requestOptions)
   .then((response) => response.json())
-  .then((data)=> {
+  .then((data) => {
     console.log(data)
     var resultList = document.createElement("ul");
     var rate = data.rates[currSymb]
@@ -26,7 +27,7 @@ function searchApi(currBase, currSymb) {
   })
 }
 
-function formSubmit(event) {
+function formRateSubmit(event) {
   event.preventDefault();
 
   var currBase = document.getElementById("currencyBase").value;
@@ -35,12 +36,32 @@ function formSubmit(event) {
   searchApi(currBase, currSymb);
 }
 
+// Handles all convert operations 
+
+function searchApiConvert(convertAmount, convertFrom, convertTo) {
+  fetch("https://api.apilayer.com/exchangerates_data/convert?to="+ convertTo +"&from="+ convertFrom +"&amount="+ convertAmount, requestOptions)
+  .then((response)=> response.json())
+  .then((data) => {
+    console.log(data)
+  })
+}
+
+function formConvertSubmit(event) {
+  event.preventDefault();
+
+  var convertAmount = document.getElementById("convertAmount").value;
+  var convertFrom = document.getElementById("convertFrom").value;
+  var convertTo = document.getElementById("convertTo").value;
+
+  searchApiConvert(convertAmount, convertFrom, convertTo);
+}
 
 
 
+// Form submisson eventListeners
 
-submitForm.addEventListener("submit", formSubmit)
-
+submitForm.addEventListener("submit", formRateSubmit);
+convertSubmit.addEventListener("submit", formConvertSubmit);
 
 
  
