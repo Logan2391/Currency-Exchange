ba70641a791b3182fa6a1cbe62ba8d0bf6876e28
Combatxd <logan.schoerner@gmail.com>
2022-07-24 10:15:26 -0600

    ok

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,16 @@
+var myHeaders = new Headers();
+myHeaders.append("apikey", "0SLoI4nb45Yf4jdHudgiFS0IMLWXY3Uq");
 
- 
+var requestOptions = {
+  method: 'GET',
+  redirect: 'follow',
+  headers: myHeaders
+};
 
+fetch("https://api.apilayer.com/exchangerates_data/latest?symbols=EUR&base=USD", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
   
 
 
